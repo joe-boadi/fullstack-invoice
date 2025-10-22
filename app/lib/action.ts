550be8e0cc6d01809e7e1551c2374cfa5f94aaed
joe-boadi <joeboadi1@gmail.com>
2025-10-22 22:15:54 +0000

    feat: handle errors with not-found and error files to fallback to.

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -17,16 +17,24 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({id: true, date: true });
 export async function createInvoice(formData: FormData) {
     const {customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
     })
+
     const amountInCents = amount * 100;
     const date =  new Date().toISOString().split('T')[0];
 
-    await sql`
+    try {
+      await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    } catch (error) {
+      console.error(error)
+      return {
+        message: 'Database Error: Failed to create invoice.'
+      }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -43,12 +51,20 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}`;
- 
+
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}`;
+    
+  } catch (error) {
+    console.error(error)
+    return {
+      message: 'Database Error: Failed to update invoice'
+    }
+  }
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
